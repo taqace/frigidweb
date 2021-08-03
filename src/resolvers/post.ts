71794513a6addf0e9285a48b7e52fa1d921499cd
taq <taqace@gmail.com>
2021-08-03 07:08:50 -0400

    post resolver and auth middleware

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,17 @@
 import { Post } from "../entities/Post";
 
-import { Resolver, Query,Arg, Mutation } from "type-graphql";
+import { Resolver, Query,Arg, Mutation, InputType, Field, Ctx, UseMiddleware } from "type-graphql";
+import { MyContext } from "../types";
+import { isAuth } from "../middleware/isAuth";
+
+
+@InputType()
+class PostInput {
+    @Field()
+    title: string
+    @Field()
+    text: string
+}
 
 @Resolver()
 export class PostResolver {
@@ -15,9 +26,15 @@ export class PostResolver {
     }
 
     @Mutation(() => Post)
-    async createPost(@Arg("title") title: string): Promise<Post> {
-        // 2 sql queries
-        return Post.create({ title }).save();
+    @UseMiddleware(isAuth)
+    async createPost(
+        @Arg("input") input: PostInput,
+        @Ctx() {req}: MyContext
+        ): Promise<Post> {
+        return Post.create({
+            ...input,
+            creatorId: req.session.userID
+        }).save();
     }
 
     @Mutation(() => Post, { nullable: true })
