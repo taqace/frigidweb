9c9db0d41bbb1d99503a159617660633eeb319a6
taq <taqace@gmail.com>
2022-01-18 22:52:45 -0800

    cookie auth cache + SSR

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -94,13 +94,18 @@ let PostResolver = class PostResolver {
             return true;
         });
     }
-    posts(limit, cursor) {
+    posts(limit, cursor, { req }) {
         return __awaiter(this, void 0, void 0, function* () {
             const realLimit = Math.min(50, limit);
             const realLimitPlusOne = realLimit + 1;
             const replacements = [realLimitPlusOne];
+            if (req.session.userID) {
+                replacements.push(req.session.userID);
+            }
+            let cursorIdx = 3;
             if (cursor) {
                 replacements.push(new Date(parseInt(cursor)));
+                cursorIdx = replacements.length;
             }
             const posts = yield typeorm_1.getConnection().query(`
 
@@ -111,10 +116,13 @@ let PostResolver = class PostResolver {
                 'email', u.email,
                 'createdAt', u."createdAt",
                 'updatedAt', u."updatedAt"
-                ) creator
+                ) creator,
+            ${req.session.userID
+                ? '(select value from updoot where "userID" = $2 and "postId" = p.id) "voteStatus"'
+                : 'null as "voteStatus"'}
             from post p
             inner join public.user u on u.id = p."creatorId"
-            ${cursor ? `where p."createdAt" < $2` : ""}
+            ${cursor ? `where p."createdAt" < $${cursorIdx}` : ""}
             order by p."createdAt" DESC
             limit $1
         `, replacements);
@@ -173,8 +181,9 @@ __decorate([
     type_graphql_1.Query(() => PaginatedPosts),
     __param(0, type_graphql_1.Arg("limit", () => type_graphql_1.Int)),
     __param(1, type_graphql_1.Arg("cursor", () => String, { nullable: true })),
+    __param(2, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:paramtypes", [Number, Object, Object]),
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "posts", null);
 __decorate([
